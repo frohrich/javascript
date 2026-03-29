fac1ff25fe45b4fc008e30b28293eb6047bc4a7c
frohrich <107487678+frohrich@users.noreply.github.com>
2026-03-29 16:02:01 -0300

    Update ex012.js

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,9 +1,13 @@
-let hora = 20
+let agora = new Date()
+let hora = agora.getHours()
+//let hora = 13
 console.log(`Agora são extamente ${hora} horas.`)
-if (hora < 12) {
+if (hora >= 6 && hora < 12) {
     console.log('Bom dia!')
-} else if (hora <= 18) {
+} else if (hora >= 12 && hora < 18) {
     console.log('Boa tarde!')
-} else {
+} else if (hora >= 18){
     console.log('Boa noite!')
+} else {
+    console.log('Boa madrugada')
 }
