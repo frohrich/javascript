daf184e9443e06d3291f71ef470afe955503a715
frohrich <107487678+frohrich@users.noreply.github.com>
2026-03-29 20:00:46 -0300

    mostrando a hora do dia

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    let msg = document.querySelector('#msg')
+    let img = document.querySelector('#imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = '#E8F0EC'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#2C1A0B'
+    } else {
+        // BOA NOITE!
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = '#0D1B2A'
+    }
+}
