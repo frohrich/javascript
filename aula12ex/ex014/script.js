67eb09db15b08dcd1a08db315059cb83be37b90f
frohrich <107487678+frohrich@users.noreply.github.com>
2026-03-30 20:13:30 -0300

    melhorando o ex

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,19 +1,17 @@
 function carregar() {
     let msg = document.querySelector('#msg')
     let img = document.querySelector('#imagem')
-    let data = new Date()
-    let hora = data.getHours()  
+    let hora = new Date().getHours()
+
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        // BOM DIA!
+
+    if (hora < 12) {
         img.src = 'fotomanha.jpg'
         document.body.style.background = '#E8F0EC'
-    } else if (hora >= 12 && hora < 18) {
-        // BOA TARDE!
+    } else if (hora < 18) {
         img.src = 'fototarde.jpg'
         document.body.style.background = '#2C1A0B'
     } else {
-        // BOA NOITE!
         img.src = 'fotonoite.jpg'
         document.body.style.background = '#0D1B2A'
     }
